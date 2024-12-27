fee6928619bd0318885830649675f9b9b1ca3647
Hector_Martil <hectormartilb@gmail.com>
2024-12-27 11:38:58 -0600

    v1.51

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,7 @@
-// Skills.jsx con Soporte para Modo Oscuro
+// Skills.jsx con soporte para Modo Oscuro y tecnologías adicionales
 import React from "react";
 import { useTranslation } from 'react-i18next';
+import { FaEnvelope } from 'react-icons/fa';
 
 const Skills = () => {
     const { t } = useTranslation();
@@ -18,7 +19,7 @@ const Skills = () => {
                     {t('skillsDescription')}
                 </p>
                 <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
-                    {/* Habilidad 1: HTML5 */}
+                    {/* Lenguajes */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
@@ -27,7 +28,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">HTML5</h3>
                     </div>
-                    {/* Habilidad 2: CSS3 */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
@@ -36,7 +36,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">CSS3</h3>
                     </div>
-                    {/* Habilidad 3: JavaScript */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
@@ -45,7 +44,7 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">JavaScript</h3>
                     </div>
-                    {/* Habilidad 4: Bootstrap */}
+                    {/* Frameworks y Librerías */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
@@ -54,7 +53,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">Bootstrap</h3>
                     </div>
-                    {/* Habilidad 5: React */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
@@ -63,7 +61,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">React</h3>
                     </div>
-                    {/* Habilidad 6: Tailwind CSS */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
@@ -72,7 +69,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">Tailwind CSS</h3>
                     </div>
-                    {/* Habilidad 7: Node.js */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
@@ -81,25 +77,7 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">Node.js</h3>
                     </div>
-                    {/* Habilidad 8: Firebase */}
-                    <div className="flex flex-col items-center">
-                        <img
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
-                            alt="Firebase"
-                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110"
-                        />
-                        <h3 className="mt-4 text-lg font-semibold">Firebase</h3>
-                    </div>
-                    {/* Habilidad 9: ChatGPT */}
-                    <div className="flex flex-col items-center">
-                        <img
-                            src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/openai.svg"
-                            alt="ChatGPT"
-                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110 dark:filter dark:invert"
-                        />
-                        <h3 className="mt-4 text-lg font-semibold">ChatGPT</h3>
-                    </div>
-                    {/* Habilidad 10: Git */}
+                    {/* Herramientas */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
@@ -108,7 +86,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">Git</h3>
                     </div>
-                    {/* Habilidad 11: GitHub */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
@@ -117,7 +94,6 @@ const Skills = () => {
                         />
                         <h3 className="mt-4 text-lg font-semibold">GitHub</h3>
                     </div>
-                    {/* Habilidad 12: Visual Studio Code */}
                     <div className="flex flex-col items-center">
                         <img
                             src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
@@ -125,6 +101,52 @@ const Skills = () => {
                             className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110"
                         />
                         <h3 className="mt-4 text-lg font-semibold">Visual Studio Code</h3>
+                    </div>
+                    {/* Backend y Servicios */}
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
+                            alt="Firebase"
+                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110"
+                        />
+                        <h3 className="mt-4 text-lg font-semibold">Firebase</h3>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/express.svg"
+                            alt="Express"
+                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110 dark:filter dark:invert"
+                        />
+                        <h3 className="mt-4 text-lg font-semibold">Express</h3>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/twilio.svg"
+                            alt="Twilio"
+                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110 dark:filter dark:invert"
+                        />
+                        <h3 className="mt-4 text-lg font-semibold">Twilio</h3>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <FaEnvelope className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110" />
+                        <h3 className="mt-4 text-lg font-semibold">EmailJS</h3>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/vercel.svg"
+                            alt="Vercel"
+                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110 dark:filter dark:invert"
+                        />
+                        <h3 className="mt-4 text-lg font-semibold">Vercel</h3>
+                    </div>
+                    {/* IA */}
+                    <div className="flex flex-col items-center">
+                        <img
+                            src="https://cdn.jsdelivr.net/npm/simple-icons@v9/icons/openai.svg"
+                            alt="ChatGPT"
+                            className="w-14 h-14 transition-transform duration-300 ease-in-out hover:scale-110 dark:filter dark:invert"
+                        />
+                        <h3 className="mt-4 text-lg font-semibold">ChatGPT</h3>
                     </div>
                 </div>
             </div>
